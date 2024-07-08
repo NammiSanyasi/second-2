43257b0157c55b9da23a9bda196c6c9fcd05f662
NammiSanyasi <nammisanyasi4a@gmail.com>
2024-07-08 20:04:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Second';
+  n = 'prasad';
+  img =
+    'https://fdn.gsmarena.com/imgroot/news/23/09/samsung-galaxy-s24-design-renders/inline/-1200/gsmarena_002.jpg';
+  title = 'Samsung S24';
+  price = '79,999';c=0
+  isNotAvailable = false;
+  fun()
+  {
+    alert("button clicked")
+  }
+  incr()
+  {
+
+  }
 }
