24230529f4fa2e2eb895ab15827137e362c501e8
NammiSanyasi <nammisanyasi4a@gmail.com>
2024-07-09 17:29:23 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  n = 'prasad';
   img =
     'https://fdn.gsmarena.com/imgroot/news/23/09/samsung-galaxy-s24-design-renders/inline/-1200/gsmarena_002.jpg';
   title = 'Samsung S24';
